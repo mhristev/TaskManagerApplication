43d9f2a623b9b9cc3c6d066844b3715e11b22129
mhristev <mhristev03@gmail.com>
2021-12-17 20:08:34 +0200

    start login

--- TaskManagerApplication/AppDelegate.swift
+++ TaskManagerApplication/AppDelegate.swift
@@ -6,16 +6,23 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate{
+    //var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        //print("APPPPPP PRINTTTT")
+        //test()
+        
         // Override point for customization after application launch.
         return true
     }
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
@@ -29,6 +36,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    
+    
+    // GOOGLE SIGN IN
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
     }
 
 
